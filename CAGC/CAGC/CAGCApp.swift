a753b074267839364582a2a7cd280e3031e2fee7
Abraham Gonzalez Diaz <143460465+gonzabbb@users.noreply.github.com>
2025-12-14 12:55:11 -0500

    IOS version of CAGC

---
+++ CAGC/CAGC/CAGCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAGCApp.swift
+//  CAGC
+//
+//  Created by Abraham Gonzalez on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CAGCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
